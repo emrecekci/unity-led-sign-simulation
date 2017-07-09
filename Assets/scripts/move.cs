a51a950366cc469e8c9f65753b44aa005d5abf92
emrecekci <emre_cekci@hotmail.com>
2017-07-09 18:46:26 +0300

    -
    -

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	public float speed=-5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+	void OnTriggerEnter(Collider temas)
+	{
+		if (temas.gameObject.tag == "tetik")
+		{
+			speed = -1 * speed;
+			Debug.Log ("dokundu");
+		}
+			
+	}
+	void Update () {
+		transform.position += new Vector3 (speed, 0, 0);
+	}
+}
---
+++ Assets/scripts/text.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class text : MonoBehaviour {
+	public GameObject text_gameObject;
+	public Text myText1;
+	string newString;
+	public Color newColor;
+	public InputField myInput;
+	Animation AnimatorComponent;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void tikla()
+	{
+		
+		myText1.text = myInput.text;
+
+		text_gameObject.SetActive(true);
+
+	}
+}
